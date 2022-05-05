e89ad021458040205e539845a45498aee0771be7
Prerak Malik <55681131+prerak01@users.noreply.github.com>
2022-05-05 20:16:59 +0530

    Add files via upload

--- reasoner.py
+++ reasoner.py
@@ -10,8 +10,11 @@ def readFile(inputFilePath):
 def reasoner1(graph):
 	# takes care of 
 	i=0
-	
+	# for subject,predicate,obj in graph:
+	# 	print(str(predicate))
+	# return;
 	rangeDictionary={}
+	# range property
 	for subject,predicate,obj in graph:
 		if(str(predicate)=="http://www.w3.org/2000/01/rdf-schema#range"):
 			rangeDictionary[str(subject)]=str(obj)
@@ -20,6 +23,7 @@ def reasoner1(graph):
 			print(str(obj),RDF.type,rangeDictionary[str(predicate)])
 
 	domainDictionary={}
+	# domain property
 	for subject,predicate,obj in graph:
 		if str(predicate)=='http://www.w3.org/2000/01/rdf-schema#domain':
 			domainDictionary[str(subject)]=str(obj)
@@ -27,8 +31,15 @@ def reasoner1(graph):
 		if str(predicate) in domainDictionary:
 			print("hey oh")
 			print(str(subject),RDF.type,domainDictionary[str(predicate)])
-
-			
+	# every subject,object is a resource property
+	for subject,predicate,obj in graph:
+		print(str(subject),RDF.type,RDFS.Resource)
+		print(str(obj),RDF.type,RDFS.Resource)
+	# every class is its own subclass
+	
+	for subject,predicate,obj in graph:
+		if(str(obj)=="http://www.w3.org/2000/01/rdf-schema#Class"):
+			print(str(subject),RDFS.subClassOf,str(subject))
 
 
 reasoner1(readFile("sampleinput.ttl"))
