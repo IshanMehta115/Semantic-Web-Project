405600d5ed7034aeb52e99d0f72b5f8acba0bb30
Ishan Mehta <73947463+IshanMehta115@users.noreply.github.com>
2022-05-13 16:53:08 +0530

    Add files via upload
    Corrected output format

--- rdf_start_with_multiple_graphs.py
+++ rdf_start_with_multiple_graphs.py
@@ -43,7 +43,7 @@ def process_ntriple(triple_string, graph_name):
     print("triple processed")
     print("following statements are added ->")
     for i in temp_statements:
-        print(i)
+        print(i + " .")
     statements[graph_name]+=temp_statements
 
 
@@ -54,18 +54,7 @@ def print_statements(graph_name):
         return
     print("\nstatements in " + graph_name + " are ->\n")
     for i in statements[graph_name]:
-        print(i)
-
-def isTriple(triple_string, graph_name):
-    if graph_name not in triple_to_tripleId:
-        print("graph with name " +graph_name + " not found")
-        return
-    else:
-        if triple_string in triple_to_tripleId[graph_name]:
-            print("given triple exists in graph " + graph_name)
-            print("triple id is " + triple_to_tripleId[graph_name][triple_string])
-        else:
-            print("given triple does not exist in graph " + graph_name)
+        print(i + " .")
 
 def process(string,level,graph_name):
     global temp_statements
@@ -84,16 +73,14 @@ def process(string,level,graph_name):
             triple_id = triple_to_tripleId[graph_name][org_string]
 
         values = string.split(' ')
-        temp_statements.append(triple_id + " subject " + values[0])
-        temp_statements.append(triple_id + " predicate " + values[1])
-        temp_statements.append(triple_id + " object " + values[2])
+        temp_statements.append(":"+triple_id + " subject " + values[0])
+        temp_statements.append(":"+triple_id + " predicate " + values[1])
+        temp_statements.append(":"+triple_id + " object " + values[2])
 
 
     else:
 
         
-
-
         starting_index = string.find("<<")
         ending_index = string.rfind(">>")
 
@@ -127,13 +114,13 @@ def process(string,level,graph_name):
 
 
             if level > 1:
-                temp_statements.append(triple_to_tripleId[graph_name][subject] + " " + predicate + " "  + object  )
-                temp_statements.append(triple_id + " subject " + triple_to_tripleId[graph_name][subject])
-                temp_statements.append(triple_id + " predicate " + predicate)
-                temp_statements.append(triple_id + " object " + object)
+                temp_statements.append(":"+triple_to_tripleId[graph_name][subject] + " " + predicate + " "  + object  )
+                temp_statements.append(":"+triple_id + " subject " + ":"+triple_to_tripleId[graph_name][subject])
+                temp_statements.append(":"+triple_id + " predicate " + predicate)
+                temp_statements.append(":"+triple_id + " object " + object)
 
             else:
-                temp_statements.append(triple_to_tripleId[graph_name][subject] + " " + predicate + " "  + object  )
+                temp_statements.append(":"+triple_to_tripleId[graph_name][subject] + " " + predicate + " "  + object  )
 
 
 
@@ -168,13 +155,13 @@ def process(string,level,graph_name):
 
 
             if level > 1:
-                temp_statements.append(subject + " " + predicate + " "  + triple_to_tripleId[graph_name][object]  )
-                temp_statements.append(triple_id + " subject " + subject)
-                temp_statements.append(triple_id + " predicate " + predicate)
-                temp_statements.append(triple_id + " object " + triple_to_tripleId[graph_name][object])
+                temp_statements.append(subject + " " + predicate + " "  + ":"+triple_to_tripleId[graph_name][object]  )
+                temp_statements.append(":"+triple_id + " subject " + subject)
+                temp_statements.append(":"+triple_id + " predicate " + predicate)
+                temp_statements.append(":"+triple_id + " object " + ":"+triple_to_tripleId[graph_name][object])
 
             else:
-                temp_statements.append(subject + " " + predicate + " "  + triple_to_tripleId[graph_name][object]  )
+                temp_statements.append(subject + " " + predicate + " "  + ":"+triple_to_tripleId[graph_name][object]  )
 
 
 
@@ -221,13 +208,13 @@ def process(string,level,graph_name):
 
 
             if level > 1:
-                temp_statements.append(triple_to_tripleId[graph_name][subject] + " " + predicate + " "  + triple_to_tripleId[graph_name][object]  )
-                temp_statements.append(triple_id + " subject " + triple_to_tripleId[graph_name][subject])
-                temp_statements.append(triple_id + " predicate " + predicate)
-                temp_statements.append(triple_id + " object " + triple_to_tripleId[graph_name][object])
+                temp_statements.append(":"+triple_to_tripleId[graph_name][subject] + " " + predicate + " "  + ":"+triple_to_tripleId[graph_name][object]  )
+                temp_statements.append(":"+triple_id + " subject " + ":"+triple_to_tripleId[graph_name][subject])
+                temp_statements.append(":"+triple_id + " predicate " + predicate)
+                temp_statements.append(":"+triple_id + " object " + ":"+triple_to_tripleId[graph_name][object])
 
             else:
-                temp_statements.append(triple_to_tripleId[graph_name][subject] + " " + predicate + " "  + triple_to_tripleId[graph_name][object]  )
+                temp_statements.append(":"+triple_to_tripleId[graph_name][subject] + " " + predicate + " "  + ":"+triple_to_tripleId[graph_name][object]  )
 
 
 
@@ -244,8 +231,7 @@ while run:
     print("0) To quit press 0")
     print("1) To insert N-Triple press 1")
     print("2) To get all statements in graph press 2")
-    print("3) To check if triple exists in a graph press 3")
-    print("4) To insert N-quad press 4")
+    print("3) To insert N-quad press 3")
 
     ch = input()
 
@@ -254,20 +240,16 @@ while run:
         graph_name = input()
         print("Enter N-triple")
         triple_string = input()
+        triple_string = "<< " + triple_string + " >>"
         process_ntriple(triple_string, graph_name)
     elif ch=="2":
         print("Enter graph name")
         graph_name = input()
         print_statements(graph_name)
     elif ch=="3":
-        print("Enter graph name")
-        graph_name = input()
-        print("Enter N-triple")
-        triple_string = input()
-        isTriple(triple_string, graph_name)
-    elif ch=="4":
         print("Enter N-quad")
         quad_string = input()
+        quad_string = "<< "  + quad_string + " >>"
         process_nquad(quad_string)
         
     elif ch=="0":
